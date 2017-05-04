fd678b9cdb8ddbdd20dcc4fff6c20d8844275304
Вова <vovanychkov95@gmail.com>
2017-05-04 17:28:06 +0300

    redactMatches1

--- app/src/main/java/com/diplom/mytournament/activities/MainActivity.java
+++ app/src/main/java/com/diplom/mytournament/activities/MainActivity.java
@@ -34,14 +34,14 @@ public class MainActivity extends AppCompatActivity
         getSupportFragmentManager().beginTransaction()
                 .add(R.id.profile_frame_layout,competitionsFragment).commit();
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+       // FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+/*        fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });
+        });*/
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
