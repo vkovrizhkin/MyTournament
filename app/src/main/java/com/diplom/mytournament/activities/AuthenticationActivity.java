dd898d79208055446f750e25d52ce557d719fe6a
Вова <vovanychkov95@gmail.com>
2017-05-11 15:00:19 +0300

    New changelist

---
+++ app/src/main/java/com/diplom/mytournament/activities/AuthenticationActivity.java
@@ -0,0 +1,111 @@
+package com.diplom.mytournament.activities;
+
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.diplom.mytournament.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class AuthenticationActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
+    @BindView(R.id.registration_button)
+    Button reg;
+
+    @BindView(R.id.sign_in_button)
+    Button sign;
+
+    @BindView(R.id.email_edit)
+    EditText emailET;
+
+    @BindView(R.id.password_edit)
+    EditText passwordET;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_authentication);
+        ButterKnife.bind(this);
+
+
+
+        reg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = emailET.getText().toString();
+                String password = passwordET.getText().toString();
+                signing(email, password);
+
+            }
+        });
+
+        sign.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = emailET.getText().toString();
+                String password = passwordET.getText().toString();
+                registration(email, password);
+
+            }
+        });
+
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+
+                } else {
+                    // User is signed out
+                }
+                // ...
+            }
+        };
+    }
+
+    public void signing(String email, String password){
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Toast.makeText(AuthenticationActivity.this, "Успешно вошёл!", Toast.LENGTH_SHORT);
+                } else{
+                    Toast.makeText(AuthenticationActivity.this, "ну ты и долбоёб, братишка!", Toast.LENGTH_SHORT);
+                }
+
+            }
+        });
+
+    }
+    public void registration(String email, String password){
+        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Toast.makeText(AuthenticationActivity.this, "Успешно зарегался!!", Toast.LENGTH_SHORT);
+                } else{
+                    Toast.makeText(AuthenticationActivity.this, "ну ты и долбоёб, братишка!", Toast.LENGTH_SHORT);
+                }
+            }
+        });
+
+    }
+
+}
