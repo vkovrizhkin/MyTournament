473c295fdeff22a6df3479c05af18266f57da60d
Вова <vovanychkov95@gmail.com>
2017-05-12 22:50:56 +0300

    AddingTeamFragment

---
+++ app/src/main/java/com/diplom/mytournament/fragments/details_redact/CreateTeamFragment.java
@@ -0,0 +1,34 @@
+package com.diplom.mytournament.fragments.details_redact;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.diplom.mytournament.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class CreateTeamFragment extends Fragment {
+
+    private String kindOfSport;
+
+    public CreateTeamFragment() {
+        // Required empty public constructor
+    }
+
+    public CreateTeamFragment(String kindOfSport) {
+        this.kindOfSport = kindOfSport;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_create_team, container, false);
+    }
+
+}
