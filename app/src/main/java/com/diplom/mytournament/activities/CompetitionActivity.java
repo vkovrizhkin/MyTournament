f21b40eb50afbff2d002da4be3f79b5297ff5f2a
Вова <vovanychkov95@gmail.com>
2017-05-29 00:48:33 +0300

    лого даже в выдвижной панельке

--- app/src/main/java/com/diplom/mytournament/activities/CompetitionActivity.java
+++ app/src/main/java/com/diplom/mytournament/activities/CompetitionActivity.java
@@ -1,7 +1,10 @@
 package com.diplom.mytournament.activities;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -11,14 +14,22 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.diplom.mytournament.MyTournamentQueryHelper;
 import com.diplom.mytournament.fragments.PlayersDialogFragment.PlayersInterface;
 import com.diplom.mytournament.R;
 import com.diplom.mytournament.fragments.details_redact.MatchDetailFragment;
 import com.diplom.mytournament.fragments.drawer.MatchesFragment;
 import com.diplom.mytournament.fragments.drawer.StandingsFragment;
 import com.diplom.mytournament.fragments.drawer.TeamsFragment;
+import com.diplom.mytournament.models.Competition;
 import com.diplom.mytournament.models.Player;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class CompetitionActivity extends AppCompatActivity
         implements PlayersInterface, NavigationView.OnNavigationItemSelectedListener {
@@ -48,10 +59,37 @@ public class CompetitionActivity extends AppCompatActivity
         drawer.setDrawerListener(toggle);
         toggle.syncState();
 
+        MyTournamentQueryHelper qh = new MyTournamentQueryHelper(this);
+        Competition competition = qh.getCompetitionById(competitionId);
+
+
+
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        View hView =  navigationView.getHeaderView(0);
+        ImageView comp_logo = (ImageView) hView.findViewById(R.id.imageView);
+        TextView title = (TextView)hView.findViewById(R.id.nav_title);
+        title.setText(competition.getName());
+        if (competition.getLogoIdResource()!=null){
+            Bitmap img = null;
+            Uri uri = Uri.parse(competition.getLogoIdResource());
+
+            try {
+                img = MediaStore.Images.Media.getBitmap(this.getContentResolver(), uri);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            comp_logo.setImageBitmap(img);
+        } else {
+            comp_logo.setImageResource(R.drawable.ic_menu_camera);
+        }
+
         navigationView.setNavigationItemSelectedListener(this);
 
         TeamsFragment teamsFragment = new TeamsFragment(competitionId);
+
+
 
         getSupportFragmentManager().beginTransaction().add(R.id.competition_frame_layout, teamsFragment)
                 .commit();
